879bb58f53984e1e72de4ecf849e8393cfdd85da
GiovanniConti <giovanni.conti@live.it>
2021-10-08 17:34:04 +0200

    Added check on entered values

--- js/script.js
+++ js/script.js
@@ -3,19 +3,29 @@ const userAge = prompt("Qual è la tua età?");
 
 const ticketPrice = (userKm * 0.21);
 
-
 let finalPrice;
+// console.log(userKm)
+// console.log(typeof(userKm))
+// console.log(isNaN(userKm))
 
-// console.log(ticketPrice)
+if (isNaN(userKm) || userKm < 0){
 
-if (userAge > 65) {
-    finalPrice = ((ticketPrice * 60) / 100)
-} else if ( userAge < 18){
-    finalPrice = ((ticketPrice * 80) / 100)
+    alert("Valore Km inserito non valido.")
+
+} else if (isNaN(userAge) || userAge < 0) {
+
+    alert("Valore Età inserito non valido.")
+
 } else {
-    finalPrice = ticketPrice;
+
+    if (userAge > 65) {
+        finalPrice = ((ticketPrice * 60) / 100)
+    } else if ( userAge < 18){
+        finalPrice = ((ticketPrice * 80) / 100)
+    } else {
+        finalPrice = ticketPrice;
+    }
 }
 
-
 // console.log(finalPrice)
-alert("Il prezzo del biglietto è: " + finalPrice.toFixed(2))
+alert("Il prezzo del biglietto è: " + finalPrice.toFixed(2) + "€")
